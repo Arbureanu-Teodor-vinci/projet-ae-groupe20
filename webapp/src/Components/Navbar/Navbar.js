59b089b63ecf46793f04944bb163cd5d923bf57c
Jason Chu <jason.chu@student.vinci.be>
2024-02-27 15:45:01 +0100

    feat:add first try login page

--- webapp/src/Components/Navbar/Navbar.js
+++ webapp/src/Components/Navbar/Navbar.js
@@ -1,2 +1,28 @@
-import Navigate from '../Router/Navigate';
-import { clearAuthenticatedUser, getAuthenticatedUser } from '../../utils/auths.js';
+// import Navigate from '../Router/Navigate';
+// import { clearAuthenticatedUser, getAuthenticatedUser } from '../../utils/auths.js';
+
+const Navbar = () => {
+    renderNavbar();
+};
+
+function renderNavbar() {
+    const navbarWrapper = document.querySelector('#navbarWrapper');
+    navbarWrapper.innerHTML = `
+    <nav class="navbar navbar-expand-sm navbar-light" id="UpPage">
+
+      <div class="d-flex justify-content-between w-100 pt-3">
+         <div class="nav nav-underline ps-5">
+          <a class="nav-link" style="font-size: 20px" href="#" data-uri="/">Accueil</a> 
+        </div>
+
+        <div class="nav nav-underline pe-5">
+        <a class="nav-link" style="font-size: 20px" href="#" data-uri="/login">Se connecter</a>
+          <a class="nav-link" style="font-size: 20px" href="#" data-uri="/register">S'inscrire</a>
+        </div>
+      </div>
+    </nav>
+  `
+    
+};
+
+export default Navbar;
