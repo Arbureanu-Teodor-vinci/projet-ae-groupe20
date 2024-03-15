8ea2c7254a8ffa017fe13a13e3060c01f4428c31
Hamadi Gaddih <hamadi.gaddih@student.vinci.be>
2024-03-15 13:54:04 +0100

    feat: add first working studentPage

--- webapp/src/Components/Router/routes.js
+++ webapp/src/Components/Router/routes.js
@@ -2,12 +2,14 @@ import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import HomePage from '../Pages/HomePage';
 import ChoicePage from '../Pages/ChoicePage';
+import StudentPage from '../Pages/StudentPage';
 
 const routes = {
     '/': HomePage, // Default route, equivalent to '
     '/login': LoginPage,
     '/register': RegisterPage,
-    '/choice': ChoicePage
+    '/choice': ChoicePage,
+    '/student': StudentPage
 };
 
 export default routes;
