9617195d5c1b7d347d6be16d31e9bfe6395470a2
Hamadi Gaddih <hamadi.gaddih@student.vinci.be>
2024-04-12 11:02:42 +0200

    feat: check if the company already exist

--- webapp/src/Components/Pages/CreationCompanyPage.js
+++ webapp/src/Components/Pages/CreationCompanyPage.js
@@ -13,6 +13,16 @@ const creationCompanyPage = async () => {
 };
 
 async function renderCreationCompanyPage(){
+    const options = {
+        method: 'GET',
+        headers : {
+          'Content-Type': 'application/json',
+          'Authorization': `${getAuthenticatedUser().token}`
+        },
+      };
+      const response = await fetch('/api/enterprises/getAll', options);
+      const companies = await response.json();
+
     const main = document.querySelector('main');
     main.innerHTML = `
     <section>
@@ -24,11 +34,17 @@ async function renderCreationCompanyPage(){
                         <div class="form-group">
                             <label for="companyName">Nom commercial</label>
                             <input type="text" class="form-control" id="companyName" required>
+                            <small id="companyNameError" class="form-text text-danger" style="display: none;">Indiquez une appellation</small>
                         </div>
                         <div class="form-group">
                             <label for="companyAppellation">Appellation *</label>
-                            <input type="text" class="form-control" id="companyAppellation">
-                            <small class="form-text text-muted">L'appellation n'est obligatoire que si le nom de l'entreprise est déjà connu dans le système.</small>
+                            <input type="text" class="form-control" id="companyAppellation" required>
+                            <small id="appellationError" class="form-text text-danger" style="display: none;">Appellation est obligatoire</small>
+                            <small id="appelationOptionnal" class="form-text text-muted">L'appellation n'est obligatoire que si le nom de l'entreprise est déjà connu dans le système.</small>
+                        </div>
+                        <div class="form-group">
+                            <labem for="comapanyCity">Ville</label>
+                            <input type="text" class="form-control" id="companyCity" required>
                         </div>
                         <div class="form-group">
                             <label for="companyAddress">Adresse</label>
@@ -55,7 +71,28 @@ async function renderCreationCompanyPage(){
         companyName = companyName.toLowerCase().replace(/\s/g, '');
         // eslint-disable-next-line no-console
         console.log(companyName);
+        // eslint-disable-next-line no-plusplus
+        const companyNameError = document.getElementById('companyNameError');
+        const appellationError = document.getElementById('appellationError');
+        const appellationInput = document.getElementById('companyAppellation');
+        const appelationNonObligatoire = document.getElementById('appelationOptionnal');
+        companyNameError.style.display = 'none'; // cachez le message d'erreur par défaut
+        appellationError.style.display = 'none'; // cachez le message d'erreur par défaut
+        appelationNonObligatoire.style.display = 'block'; 
+        appellationInput.required = false;
         
+        // eslint-disable-next-line no-plusplus
+        for(let i = 0; i < companies.length; i++){
+            // eslint-disable-next-line prefer-const
+            let tradeName = companies[i].tradeName.toLowerCase().replace(/\s/g, '');
+            if(tradeName === companyName){
+                companyNameError.style.display = 'block';
+                appellationError.style.display = 'block'; // affichez le message d'erreur si le nom de l'entreprise existe déjà
+                appelationNonObligatoire.style.display = 'none'; // cachez le message d'erreur par défaut
+                appellationInput.required = true;
+                break;
+            }
+        }
     });
 }
 
