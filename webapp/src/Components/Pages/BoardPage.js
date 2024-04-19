98a1c9dd9b4d7a98148a4d3f943eb0aebd8a5cb0
Jason Chu <jason.chu@student.vinci.be>
2024-04-19 09:12:41 +0200

    feat:list-of-enterprises-frontend

--- webapp/src/Components/Pages/BoardPage.js
+++ webapp/src/Components/Pages/BoardPage.js
@@ -1,11 +1,26 @@
-import { clearPage } from "../../utils/render"
+import { clearPage } from "../../utils/render";
+import { getAuthenticatedUser } from '../../utils/auths';
 
-const ModificationPage = async () => {
+const BoardPage = async () => {
     clearPage();
     await renderBoardPage();
 }
 
 async function renderBoardPage() {
+    const options = {
+        method: 'GET',
+        headers : {
+            'Content-Type': 'application/json',
+            "Authorization": `${getAuthenticatedUser().token}`,
+        },
+    };
+
+    const response = await fetch(`/api/enterprises/getAll`, options);
+
+    const enterprises = await response.json();
+
+    
+
     const main = document.querySelector('main');
     main.innerHTML = `
     <section>
@@ -14,9 +29,41 @@ async function renderBoardPage() {
                 <div class="col-12 text-center">
                     <h1>Tableau de bord</h1>
                 </div>
+                <p>Voici la liste de tout les entreprises :</p>
             </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Entreprise</th>
+                        <th>Appelation</th>
+                        <th>Adresse</th>
+                        <th>Numéro de téléphone</th>
+                        <th>Ville</th>
+                        <th>Email</th>
+                        <th>Black-listée</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
         </div>
     </section>`;
+
+    const enterprisesTable = document.querySelector('.table tbody');
+    enterprises.forEach(enterprise => {
+        enterprisesTable.innerHTML += `
+          <tr>
+            <td>${enterprise.tradeName}</td>
+            <td>${enterprise.designation}</td>
+            <td>${enterprise.adresse}</td>
+            <td>${enterprise.phoneNumber}</td>
+            <td>${enterprise.city}</td>
+            <td>${enterprise.email}</td>
+            <td>${enterprise.blackListed}</td>
+        </tr>
+        `;
+    }
+    );
 };
 
-export default ModificationPage;
+export default BoardPage;
