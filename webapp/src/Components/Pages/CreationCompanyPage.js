a16b8b3804516b026d41e0122f748f421384ddd9
Hamadi Gaddih <hamadi.gaddih@student.vinci.be>
2024-04-19 11:28:49 +0200

    feat: frontend add a new company

--- webapp/src/Components/Pages/CreationCompanyPage.js
+++ webapp/src/Components/Pages/CreationCompanyPage.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
 import { getAuthenticatedUser } from "../../utils/auths"
 import { clearPage } from "../../utils/render";
 import Navigate from "../Router/Navigate";
@@ -20,7 +22,7 @@ async function renderCreationCompanyPage(){
           'Authorization': `${getAuthenticatedUser().token}`
         },
       };
-      const response = await fetch('/api/enterprises/getAll', options);
+      const response = await fetch(`/api/enterprises/getAll`, options);
       const companies = await response.json();
 
     const main = document.querySelector('main');
@@ -34,13 +36,14 @@ async function renderCreationCompanyPage(){
                         <div class="form-group">
                             <label for="companyName">Nom commercial</label>
                             <input type="text" class="form-control" id="companyName" required>
-                            <small id="companyNameError" class="form-text text-danger" style="display: none;">Indiquez une appellation</small>
+                            <small id="appelationIndicator" class="form-text text-danger" style="display: none;">Indiquez une appellation</small>
                         </div>
                         <div class="form-group">
                             <label for="companyAppellation">Appellation *</label>
                             <input type="text" class="form-control" id="companyAppellation" required>
-                            <small id="appellationError" class="form-text text-danger" style="display: none;">Appellation est obligatoire</small>
+                            <small id="appelationObligatoire" class="form-text text-danger" style="display: none;">Appellation est obligatoire</small>
                             <small id="appelationOptionnal" class="form-text text-muted">L'appellation n'est obligatoire que si le nom de l'entreprise est déjà connu dans le système.</small>
+                            <small id="appelationPresent" class="form-text text-danger" style="display: none;">L'appellation est déjà utilisée.</small>
                         </div>
                         <div class="form-group">
                             <labem for="comapanyCity">Ville</label>
@@ -48,7 +51,7 @@ async function renderCreationCompanyPage(){
                         </div>
                         <div class="form-group">
                             <label for="companyAddress">Adresse</label>
-                            <input type="text" class="form-control" id="companyAddress" required>
+                            <input type="text" class="form-control" id="companyAdress" required>
                         </div>
                         <div class="form-group">
                             <label for="companyEmail">Email</label>
@@ -58,42 +61,106 @@ async function renderCreationCompanyPage(){
                             <label for="companyTel">Tel</label>
                             <input type="tel" class="form-control" id="companyTel" required>
                         </div>
-                        <button type="submit" class="btn btn-success float-right mt-3">Ajouter</button>
+                        <button type="submit" class="btn btn-success float-right mt-3" id="create">Ajouter</button>
                     </form>
                 </div>
             </div>
         </div>
     </section>`;
 
-    // eslint-disable-next-line func-names
-    document.getElementById('companyName').addEventListener('input', function() {
-        let companyName = this.value;
-        companyName = companyName.toLowerCase().replace(/\s/g, '');
-        // eslint-disable-next-line no-console
-        console.log(companyName);
-        // eslint-disable-next-line no-plusplus
-        const companyNameError = document.getElementById('companyNameError');
-        const appellationError = document.getElementById('appellationError');
-        const appellationInput = document.getElementById('companyAppellation');
-        const appelationNonObligatoire = document.getElementById('appelationOptionnal');
-        companyNameError.style.display = 'none'; // cachez le message d'erreur par défaut
-        appellationError.style.display = 'none'; // cachez le message d'erreur par défaut
-        appelationNonObligatoire.style.display = 'block'; 
-        appellationInput.required = false;
-        
-        // eslint-disable-next-line no-plusplus
-        for(let i = 0; i < companies.length; i++){
-            // eslint-disable-next-line prefer-const
-            let tradeName = companies[i].tradeName.toLowerCase().replace(/\s/g, '');
-            if(tradeName === companyName){
-                companyNameError.style.display = 'block';
-                appellationError.style.display = 'block'; // affichez le message d'erreur si le nom de l'entreprise existe déjà
-                appelationNonObligatoire.style.display = 'none'; // cachez le message d'erreur par défaut
-                appellationInput.required = true;
+    const companyName = document.getElementById('companyName');
+    const designation = document.getElementById('companyAppellation');
+    const appelationIndicator = document.getElementById('appelationIndicator');
+    const appelationObligatoire = document.getElementById('appelationObligatoire');
+    const appelationOptionnal = document.getElementById('appelationOptionnal');
+    const appelationPresent = document.getElementById('appelationPresent');
+
+    appelationIndicator.style.display = 'none';
+    appelationObligatoire.style.display = 'none';
+    appelationOptionnal.style.display = 'block';
+    appelationPresent.style.display = 'none';
+
+    designation.required = false;
+
+    // Check if the company name is already in the database
+    companyName.addEventListener('input', () => {
+    for (let i = 0; i < companies.length; i++) {
+            const tradeNameData = companies[i].tradeName.toLowerCase().replace(/\s/g, '');
+            if(companyName.value.toLowerCase().replace(/\s/g, '') === tradeNameData){
+                appelationIndicator.style.display = 'block';
+                appelationObligatoire.style.display = 'block';
+                appelationOptionnal.style.display = 'none';
+                designation.required = true;
                 break;
             }
+            else{
+                appelationIndicator.style.display = 'none';
+                appelationObligatoire.style.display = 'none';
+                appelationOptionnal.style.display = 'block';
+                designation.required = false;
+            }
+        }
+
+        if(designation.required === true){
+            designation.addEventListener('input', () => {
+                for (let i = 0; i < companies.length; i++) {
+                    if(companies[i].designation){
+
+                    if(companies[i].tradeName.toLowerCase().replace(/\s/g, '') === companyName.value.toLowerCase().replace(/\s/g, '') 
+                    && companies[i].designation.toLowerCase().replace(/\s/g, '') === designation.value.toLowerCase().replace(/\s/g, '')){
+                        appelationPresent.style.display = 'block';
+                        appelationObligatoire.style.display = 'none';
+                        break;
+                    }
+                    else{
+                        appelationObligatoire.style.display = 'block';
+                        appelationPresent.style.display = 'none';
+                    }
+                }
+            }
+            })
         }
     });
-}
+
+    
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', async (event) => {
+        const companyParam = companyName.value; 
+        const designationParam = designation.value;
+        const cityParam = document.getElementById('companyCity').value;
+        const adresseParam = document.getElementById('companyAdress').value;
+        const emailParam = document.getElementById('companyEmail').value;
+        const phoneNumberParam = document.getElementById('companyTel').value;
+
+        event.preventDefault();
+        const optionsCreateCompany = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${getAuthenticatedUser().token}`,
+            },
+            body: JSON.stringify({
+                tradeName: companyParam,
+                designation : designationParam,
+                adresse : adresseParam,
+                phoneNumber : phoneNumberParam,
+                city : cityParam,
+                email : emailParam
+            }),
+            
+        };
+        
+        const responseCreateCompany = await fetch(`/api/enterprises/add`, optionsCreateCompany);
+        if(responseCreateCompany.ok){
+            event.preventDefault();
+            Navigate('/creationContact');
+        }
+        else{
+            main.innerHTML += `<div class="alert alert-danger" role="alert">Erreur lors de la création de l'entreprise</div>`;
+        }
+    });
+
+};
 
 export default creationCompanyPage;
