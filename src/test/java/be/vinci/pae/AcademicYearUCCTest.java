3ea031f19f05673dbe8129abe7dd043ee37b0571
Teodor Arbureanu <teodor.arbureanu@student.vinci.be>
2024-04-25 03:07:10 +0200

    feat: test academic year UCC

--- src/test/java/be/vinci/pae/AcademicYearUCCTest.java
+++ src/test/java/be/vinci/pae/AcademicYearUCCTest.java
@@ -1,7 +1,9 @@
 package be.vinci.pae;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import be.vinci.pae.api.filters.BusinessException;
 import be.vinci.pae.domain.academicyear.AcademicYear;
 import be.vinci.pae.domain.academicyear.AcademicYearDTO;
 import be.vinci.pae.domain.academicyear.AcademicYearUCC;
@@ -34,8 +36,6 @@ public class AcademicYearUCCTest {
     academicYearDTO.setYear("2023-2024");
     AcademicYear academicYear = (AcademicYear) academicYearDTO;
     //Mockito.when(academicYear.isActual()).thenReturn(true);
-    Mockito.when(academicYearDAO.getActualAcademicYear())
-        .thenReturn(academicYearDTO);
   }
 
   @Test
@@ -44,5 +44,61 @@ public class AcademicYearUCCTest {
     Mockito.when(academicYearDAO.getActualAcademicYear()).thenReturn(academicYearDTO);
     assertEquals(academicYearDTO, academicYearUCC.getOrAddActualAcademicYear());
   }
+
+
+  @Test
+  @DisplayName("Get or add actual academic year when academic year is not actual")
+  void getOrAddActualAcademicYearWhenNotActual() {
+    AcademicYearDTO academicYearDTONotValid = domainFactory.getAcademicYearDTO();
+    academicYearDTO.setYear("2022-2023");
+
+    Mockito.when(academicYearDAO.getActualAcademicYear()).thenReturn(academicYearDTO);
+
+    Mockito.when(academicYearDAO.addAcademicYear(Mockito.anyString())).thenReturn(academicYearDTO);
+
+    AcademicYearDTO actualAcademicYearDTO = academicYearUCC.getOrAddActualAcademicYear();
+
+    assertEquals(academicYearDTO, actualAcademicYearDTO);
+  }
+
+  @Test
+  @DisplayName("Get academic year by valid year")
+  void getAcademicYearByValidYear() {
+    String year = "2023-2024";
+    AcademicYearDTO expectedAcademicYearDTO = domainFactory.getAcademicYearDTO();
+    expectedAcademicYearDTO.setYear(year);
+
+    Mockito.when(academicYearDAO.getAcademicYearByAcademicYear(year))
+        .thenReturn(expectedAcademicYearDTO);
+
+    AcademicYearDTO actualAcademicYearDTO = academicYearUCC.getAcademicYearByYear(year);
+
+    assertEquals(expectedAcademicYearDTO, actualAcademicYearDTO);
+  }
+
+  @Test
+  @DisplayName("Get academic year by null year")
+  void getAcademicYearByNullYear() {
+    String year = null;
+
+    assertThrows(BusinessException.class, () -> academicYearUCC.getAcademicYearByYear(year));
+  }
+
+  @Test
+  @DisplayName("Get academic year by empty year")
+  void getAcademicYearByEmptyYear() {
+    String year = "";
+
+    assertThrows(BusinessException.class, () -> academicYearUCC.getAcademicYearByYear(year));
+  }
+
+  @Test
+  @DisplayName("Get academic year by invalid year format")
+  void getAcademicYearByInvalidYearFormat() {
+    String year = "2023";
+
+    assertThrows(BusinessException.class, () -> academicYearUCC.getAcademicYearByYear(year));
+  }
+
 
 }
